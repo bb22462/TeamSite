9eb1bcdf8e608e2bcedfb4ca3951734ac21013a7
IndexZeroZzz <l0rmin@yandex.com>
2023-10-14 17:14:28 +0300

    OMAGAD BUGFIXES

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,3 +1,5 @@
+import Markdown from 'react-markdown'
+
 export default function Post({title, date, article}: {title: string, date: string, article: string}) {
     return(
         <>
@@ -5,7 +7,7 @@ export default function Post({title, date, article}: {title: string, date: strin
                 <h1 className="text-2xl font-bold mb-3">{title}</h1>
                 <h3 className="text-sm font-light mb-5">{date}</h3>
                 <div>
-                    {article}
+                    <article className='prose prose-md'><Markdown>{article}</Markdown></article>
                 </div>
             </div>
         </>
