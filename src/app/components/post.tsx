a894ff1fe81e34b02a8b3ba032a7bc4c687fddab
IndexZeroZzz <l0rmin@yandex.com>
2023-12-21 18:47:06 +0300

    Updat

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -3,7 +3,7 @@ import Markdown from 'react-markdown'
 export default function Post({title, date, article}: {title: string, date: string, article: string}) {
     return(
         <>
-            <div className="flex min-h-screen flex-col p-8 content-center">
+            <div className="flex min-h-screen flex-col p-8 content-center text-center">
                 <h1 className="text-2xl font-bold mb-3">{title}</h1>
                 <h3 className="text-sm font-light mb-5">{date}</h3>
                 <div>
