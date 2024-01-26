2422991f5f7e2315372cbd3e968a20663de7206c
IndexZeroZzz <l0rmin@yandex.com>
2024-01-26 18:17:57 +0300

    Bugfixes: - Improved news view - Reversed news list

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -3,11 +3,11 @@ import Markdown from 'react-markdown'
 export default function Post({title, date, article}: {title: string, date: string, article: string}) {
     return(
         <>
-            <div className="flex min-h-screen flex-col p-8 content-center text-center">
+            <div className="flex min-h-screen flex-col p-8 content-centerW">
                 <h1 className="text-2xl font-bold mb-3">{title}</h1>
                 <h3 className="text-sm font-light mb-5">{date}</h3>
                 <div>
-                    <article className='prose prose-md'><Markdown>{article}</Markdown></article>
+                    <article style={{}} className='prose prose-md max-w-none'><Markdown>{article}</Markdown></article>
                 </div>
             </div>
         </>
