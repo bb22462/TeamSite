dce35653aafcc6b58dade96fa8609f674fd9fa07
indexzerozzz <l0rmin@yandex.com>
2024-03-28 18:15:47 +0300

    update yay

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -21,12 +21,13 @@ export default function Team() {
                         <a className="transition text-blue-500 bg-gray-950 hover:bg-gray-800 p-3 rounded-md " href="https://t.me/dy4enkolina">Telegram</a>                
                     </div>
                     <div className='grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-1 grid-rows-6'>
-                        <Member fileName="danya" name="Парамонов Даня" role="Конструктор" list={["Сборка робота", "Контроль за кабинетом", "Систематизация"]}></Member>
-                        <Member fileName="matvey" name="Калякин Матвей" role="3D-Моделист" list={["Сборка робота", "Моделирование подъемника", "Создание стратегии"]}></Member>
-                        <Member fileName="misha" name="Юхимчук Миша" role="Конструктор" list={["Создание стратегии", "Управление роботом", "Работа над роботом"]}></Member>
-                        <Member fileName="oleg" name="Калугин Олег" role="Программист" list={["Написание кода", "Работа с соцсетями", "Компьютерный мастер"]}></Member>
-                        <Member fileName="nastya" name="Муромцева Настя" role="Коммуникация" list={["Взаимодействие с командами", "Работа над портфолио", "Ведение соцсетей"]}></Member>
-                        <Member fileName="varya" name="Захарова Варя" role="Дизайнер" list={["Разработка логотипа", "Дизайн стикер-пака", "Создание флага"]}></Member>
+                        <Member fileName="danya" name="Парамонов Даня" role="Конструктор" list={[]}></Member>
+                        <Member fileName="matvey" name="Калякин Матвей" role="Конструктор" list={[]}></Member>
+                        <Member fileName="misha" name="Юхимчук Миша" role="Драйвер" list={[]}></Member>
+                        <Member fileName="oleg" name="Калугин Олег" role="Программист" list={[]}></Member>
+                        <Member fileName="varya" name="Захарова Варя" role="Дизайнер" list={[]}></Member>
+                        <Member fileName="nastya" name="Бараш Настя" role="3D-Моделист" list={[]}></Member>
+                        <Member fileName="maxim" name="Кянксеп Максим" role="Дизайнер" list={[]}></Member>
                     </div>
                 </div>
             </div>
